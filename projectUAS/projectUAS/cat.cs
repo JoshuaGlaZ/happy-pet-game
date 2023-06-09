5e1c5e83afa8242b4f8f02ea4c8c9dbbc67db3f9
AlexanderKentSo <136101534+AlexanderKentSo@users.noreply.github.com>
2023-06-09 22:35:45 +0700

    Add files via upload

---
+++ projectUAS/projectUAS/cat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectUAS
+{
+    public class cat : pet
+    {
+        #region dataMember
+        private bool vaccStatus;
+        #endregion
+
+        #region constructor
+        public cat(string name, Image image, player owner):base(name,image,owner)
+        {
+            VaccStatus = false;
+        }
+        #endregion
+
+        #region property
+        public bool VaccStatus { get => vaccStatus; set => vaccStatus = value; }
+        #endregion
+
+        #region method
+        public override string DisplayData()
+        {
+            string condition;
+            if (VaccStatus) { condition = "true"; }
+            else { condition = "false"; }
+            return base.ToString() + 
+                   "\nVaccine Status : " + condition;
+        }
+
+        public void Play()
+        {
+            base.Happiness += 50;
+            base.Energy -= 30;
+            base.Owner.Coins += (int)(0.5*50*100);
+        }
+
+        public void sleep()
+        {
+            base.Happiness += 20;
+            base.Energy += 70;
+            base.Owner.Coins += (int)(0.5 * 20 * 100);
+            base.Owner.Coins += (int)(0.5 * 70 * 100);
+        }
+
+        public void bath()
+        {
+            base.Health += 30;
+            base.Owner.Coins += (int)(0.5 * 30 * 100);
+        }
+        #endregion
+    }
+}
