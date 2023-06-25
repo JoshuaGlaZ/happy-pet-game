1a02c175f59f8e9af52da6c1854ca4a0f80fad5e
AlexanderKentSo <136101534+AlexanderKentSo@users.noreply.github.com>
2023-06-25 19:04:27 +0700

    Add files via upload
    ditambahin parent class enemy dan child class enemyPoisonous(belum lengkap), enemyDrain, enemyDebuffer. sama perbaikan di class player, pet dan turunannya.

---
+++ happy-pet-game/projectUAS_2019/happy-pet-game-2019/EnemyDrain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace happy_pet_game_2019
+{
+    public class EnemyDrain:Enemy
+    {
+        private int drainEffect;
+        public EnemyDrain(string inName, Image inPict, int inSpeed, int inHealth, int inEnergy, int inDrainEffect) : base(inName, inPict, inSpeed, inHealth, inEnergy)
+        {
+            Name = inName;
+            Image = inPict;
+            Speed = inSpeed;
+            Health = inHealth;
+            Energy = inEnergy;
+            DrainEffect = inDrainEffect;
+        }
+
+        public int DrainEffect { get => drainEffect; set => drainEffect = value; }
+
+        public override void specialAttack(Pet target)
+        {
+            base.specialAttack(target);
+            target.Happiness -= DrainEffect;
+        }
+    }
+}
